0ce2e6c0a7797c8d1e32b93d97af196f236952ec
gobbls <98641699+gobbls@users.noreply.github.com>
2026-02-06 22:27:57 +0100

    not overwriting existing files, appending zeroes for sorting

--- main.py
+++ main.py
@@ -1,22 +1,6 @@
 #!/usr/bin/env python3
 
 
-#
-# KNOWN QUIRK(s):
-#
-# 1. When files in a directory is already renamed from a first-time-run;
-#    Running the script again will still rename the files if;
-#    the files are not in the same position / index as the first run
-#    due to how unix lists files in order: 1, 10, 11, 12, ...2, 20, 21...
-#
-#    - Can be fixed by prefixing numbers with:
-#      "0" * (longest_file_name_len - current_file_name_len)
-#      
-#      But this beats the convention I want to follow here,
-#      since I usually use Thunar to browse media files anyways.
-#
-
-
 import os
 from sys import argv
 
@@ -24,7 +8,7 @@ from sys import argv
 TARGET = argv[1]
 
 
-def get_new_name(name: str, num: int) -> str:
+def get_new_name(name: str, num: int, longest_num: int) -> str:
     '''
     Takes a filename (the entire path) and simplifies
     it to a unique number as well as making the entire
@@ -41,15 +25,21 @@ def get_new_name(name: str, num: int) -> str:
     if '.' not in name:
         raise ValueError(f'[ERROR] {name} does not contain an exstension!')
 
-    extension = name.split('.')[-1].lower()
     parent = os.path.dirname(name)
-    new_name = os.path.join(parent, f'{num}.{extension}')
+    extension = name.split('.')[-1].lower()
+    new_name = str(num)
+    prefix = "0" * (longest_num - len(new_name))
+    new_full_name = os.path.join(parent, f'{prefix}{new_name}.{extension}')
 
-    return new_name
+    return new_full_name
 
 
 def recur(path: str) -> None:
-    paths = os.listdir(path)
+    # Append the found files to their parent path.
+    paths = [os.path.join(path, p) for p in os.listdir(path)]
+
+    # The longest number by literal characters.
+    longest_num = len(str(len(paths)))
 
     if paths == []:
         print(f'[WARNING] "{path}" is empty! Deleting...')
@@ -65,25 +55,23 @@ def recur(path: str) -> None:
 
     iter = 1
 
-    for p in paths:
-        p_path = os.path.join(path, p)
+    for f_path in paths:
+        if os.path.isdir(f_path):
+            recur(f_path)
 
-        if os.path.isdir(p_path):
-            recur(p_path)
-
-        elif os.path.isfile(p_path):
-            new_name = get_new_name(p_path, iter)
+        elif os.path.isfile(f_path):
+            new_name = get_new_name(f_path, iter, longest_num)
             iter += 1
 
-            if (p_path == new_name):
+            if (new_name in paths):
+                print(f"[LOG] {new_name} already exists! Continuing without taking action...")
                 continue
 
-            try:
-                os.rename(p_path, new_name)
-            except FileExistsError as e:
-                raise FileExistsError(f'[ERROR] {new_name} already exists! Got error:\n{e}')
-            except:
-                raise Exception(f"[ERROR] Unknown error while renaming {path}!")
+            if (f_path == new_name):
+                print(f"[LOG] {f_path} == {new_name} ignoring same name...")
+                continue
+
+            os.rename(f_path, new_name)
 
 
 def main() -> None:
