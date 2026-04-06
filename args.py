d59bf5d2a76951e3e0f829a0f9ff66a19389b32b
gobbls <98641699+gobbls@users.noreply.github.com>
2026-04-06 21:28:02 +0200

    Removed argument I won't be using

--- args.py
+++ args.py
@@ -1,40 +1,42 @@
 import argparse
 
-parser = argparse.ArgumentParser(
+__all__ = ["args"]
+
+_parser = argparse.ArgumentParser(
     prog="numb-renamer",
     description="Numerize all media files in given directories, recursing all child directories or not.",
 )
 
-parser.add_argument("targets")
+_parser.add_argument("targets", nargs="+")
 
-parser.add_argument(
+_parser.add_argument(
     "-p", "--padding", help="Add (zero)-padding to the filenames.", action="store_true"
 )
 
-parser.add_argument(
+_parser.add_argument(
     "-r",
     "--recursive",
     help="Recurse through child directories of given target.",
     action="store_true",
 )
 
-parser.add_argument(
-    "-c",
-    "--padding-char",
-    help="Specify the padding character (or string).",
-    default="0",
+_parser.add_argument(
+    "-d",
+    "--delete-empty-dir",
+    help="Delete empty directories we stumble uppon during runtime.",
+    action="store_true",
 )
 
-parser.add_argument(
+_parser.add_argument(
     "--pre-check",
     help="Check if the renaming process will go as planned before running the process. Usefull for large directories.",
     action="store_true",
 )
 
-parser.add_argument(
+_parser.add_argument(
     "--carry-over",
     help="Let the file-numeration carry over to the next directory, instead of resetting to zero.",
     action="store_true",
 )
 
-args = parser.parse_args()
+args = _parser.parse_args()
